f1dcab0af073a377892fd6dbc73b7c9020775333
Egon Elbre <egonelbre@gmail.com>
2020-10-30 11:16:36 +0200

    production: drafts

---
+++ content/draft/production-ready-concurrency.md
@@ -0,0 +1,17 @@
+---
+draft: true
+title: Production Ready Go Concurrency
+description: "How to write concurrent code in Go."
+date: ""
+tags: []
+---
+
+No goroutine left behind
+
+Handle context
+
+Fence
+
+Limiter
+
+Workers
---
+++ content/draft/production-ready-server.go
@@ -0,0 +1,17 @@
+---
+draft: true
+title: Production Ready Go Server
+description: "How to write server code in Go."
+date: ""
+tags: []
+---
+
+localhost vs. 127.0.0.1:8000
+localhost vs. 127.0.0.0:8000
+
+New(log Log, service *Service)
+Start(ctx context.Context)
+
+Background
+
+Shutdown
